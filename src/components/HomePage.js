30f2d69fde8a78b850c8e8c24e2faf4bc1bbadbb
Sanyog Jain <sanyogjain5678@gmail.com>
2023-04-22 13:21:38 +0530

    Try new logic for Show Detail page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,22 +12,21 @@ function HomePage() {
     let all_data = [];
     const fetchNdjson = async () => {
       /* const response = await fetch("https://api.tvmaze.com/schedule/web?date="+String(new Date().toISOString().split('T')[0])); */
-      const response = await fetch("https://api.tvmaze.com/schedule/web?date=2021-09-20");
+      const response = await fetch("https://api.tvmaze.com/schedule/web?date=2023-04-21");
       const exampleReader = ndjsonStream(response.body).getReader();
-    
+
       let result;
       while (!result || !result.done) {
         result = await exampleReader.read();
         all_data.push(result.value);
-        //console.log(result.done, result.value); //result.value is one line of your NDJSON data
       }
-      //console.log(all_data[0][5519])
       setAPIData(all_data[0]);
     }
-  
+
     useEffect(() => {
       fetchNdjson();
     },[])
+
     useEffect(() => {
       console.log(apiData)
     },[apiData])
@@ -39,21 +38,24 @@ function HomePage() {
   
             {apiData.map((item) => {
           if (item.image != null && item.summary != null){
-            
+
           return (<Grid item xs={4}>
             <Link to={`/showdetails/${item._embedded.show.id}`}>
             <TVShowCard className="tv-show-card" title={item._embedded.show.name} imageURL={item.image.medium} date={String(item.airdate+" "+item.airtime)} summary={item._embedded.show.summary.replace(/<\/?[^>]+(>|$)/g, "")} key={item.id}/>
                   </Link>
                   </Grid>)
           }
-          /* else {
-           
-            return ( <Grid item xs={4}><Link to={`/showdetails/${item.id}`}><TVShowCard mt="100px" pt="100px" className="tv-show-card" title={item.name} date={String(item.airdate+" "+item.airtime)} key={item.id}/>
+          else if(item.image != null && item.summary == null) {
+            return ( <Grid item xs={4}><Link to={`/showdetails/${item.id}`}><TVShowCard mt="100px" pt="100px" className="tv-show-card" title={item.name} imageURL={item.image.medium} date={String(item.airdate+" "+item.airtime)} key={item.id}/>
             </Link></Grid>
           )
-          } */
+          }
+          else if(item.image == null && item.summary != null) {
+            return ( <Grid item xs={4}><Link to={`/showdetails/${item.id}`}><TVShowCard mt="100px" pt="100px" className="tv-show-card" title={item.name} date={String(item.airdate+" "+item.airtime)} summary={item._embedded.show.summary.replace(/<\/?[^>]+(>|$)/g, "")} key={item.id}/>
+            </Link></Grid>
+          )
+          }
         })}
-  
         </Grid>
       </Box>
       </>
--- src/components/TVShowDetails.js
+++ src/components/TVShowDetails.js
@@ -3,37 +3,39 @@ import { useEffect, useState } from 'react';
 import TVShowDetailCard from './TVShowDetailCard';
 
 function TVShowDetails() {
+  //we need a sample initial state otherwise it doesn't work, because of react-router
     const [apiShowData, setAPIShowData] = useState([{"name":"","language":"","genres":["",""],"premiered":"","ended":"","officialSite":"","schedule":{"time":"","days":[]},"image":{"medium":"","original":""},"summary":""}]);
-    const [showID, setShowID] = useState(0);
-    //we must stream the data from the API, because it is too large and can block the browser
+    //stream data from API just like homepage
     let all_data = [];
     const fetchNdjsonShow = async () => {
       const response = await fetch("https://api.tvmaze.com/shows/"+window.location.href.split('/').pop());
       const exampleReader = ndjsonStream(response.body).getReader();
-    
+
       let result;
       while (!result || !result.done) {
         result = await exampleReader.read();
         all_data.push(result.value);
       }
-      setAPIShowData(all_data);
+      setAPIShowData(all_data);//set api data
     }
     useEffect(()=>{
-      fetchNdjsonShow()
+      fetchNdjsonShow()//call api once
     },[])
-    
 
-    useEffect(()=>{
-      setShowID(window.location.href.split('/').pop())
-    },[])
-    useEffect(() => {
-      console.log(apiShowData)
-    },[apiShowData])
-  
     return (
       <>
-        <TVShowDetailCard imageURL={apiShowData[0].image.original} title={apiShowData[0].name} summary={apiShowData[0].summary.replace(/<\/?[^>]+(>|$)/g, "")}/>
-       </>
+    {apiShowData.map((item)=>{
+        if(item.image != null){
+          return(
+        <TVShowDetailCard title={item.name} summary={item.summary.replace(/<\/?[^>]+(>|$)/g, "")}/>
+    )}
+    else {
+      return(
+        <TVShowDetailCard imageURL={item.image.original} title={item.name} summary={item.summary.replace(/<\/?[^>]+(>|$)/g, "")}/>
+    )}
+    }
+    )}
+        </>
     );
   }
 
